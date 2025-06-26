4522903469656a986f790b36e5aa1dc3dc2eda11
stanley ikegwu <stagasstanley@gmail.com>
2025-06-26 14:27:48 +0100

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+var user = {
+  username: "nigga",
+  password: "beatmyass"
+    email:"stagasstanley@gmail.com"
+}
+
+var user = {
+  username: "john",
+  password: "superSecret123"
+};
+
+var database = [
+ {
+  username: "john",
+  password: "superSecret123"
+ }
+];
+
+var newsfeed = [
+  {
+    username: "nigga",
+    timeline: "Excited to start learning JavaScript!"
+  },
+  {
+    username: "negro",
+    timeline: "Just posted my first photo!"
+  },
+  {
+    username: "eminem",
+    timeline: "Loving this new social platform."
+  }
+];
+
